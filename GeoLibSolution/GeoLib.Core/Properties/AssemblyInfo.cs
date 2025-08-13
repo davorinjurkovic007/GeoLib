c50377a64593288ff3fcaa9b70a62ff7ed892fc9
Davorin <davorin.jurkovic@gmail.com>
2025-08-13 22:24:16 +0200

    Solution first step

---
+++ GeoLibSolution/GeoLib.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GeoLib.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeoLib.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29cfdc54-79f5-49ed-96f2-680f5da10a32")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GeoLibSolution/GeoLib.Data/Entities/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.Data.Entities
+{
+    public class State : IIdentifiableEntity
+    {
+        public int StateId { get; set; }
+        public string Abbreviation { get; set; }
+        public string Name { get; set; }
+        public bool IsPrimaryState { get; set; }
+
+        public int EntityId
+        {
+            get { return StateId; }
+            set { StateId = value; }
+        }
+    }
+}
