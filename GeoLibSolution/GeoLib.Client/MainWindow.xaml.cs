1e63018c54458005cb938a483d23d2e798c28ef2
Davorin <davorin.jurkovic@gmail.com>
2025-08-18 13:38:52 +0200

    Preparing Proxies and Client configuration lesson

---
+++ GeoLibSolution/GeoLib.Client/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeoLib.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Title = "UI Running on Thread " + Thread.CurrentThread.ManagedThreadId +
+                " | Process " + Process.GetCurrentProcess().Id.ToString();
+        }
+
+        private void btnGetInfo_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtZipCode.Text != "")
+            {
+
+            }
+        }
+
+        private void btnGetZipCodes_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtState.Text != null)
+            {
+
+            }
+        }
+
+        private void btnMakeCall_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
