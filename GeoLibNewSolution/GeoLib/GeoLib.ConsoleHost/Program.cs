f41e421e2ec87235e3659f728a0273d1f0297b52
Davorin <davorin.jurkovic@gmail.com>
2025-09-11 22:24:27 +0200

    Project GeoLib.ConsoleHost

--- GeoLibNewSolution/GeoLib/GeoLib.ConsoleHost/Program.cs
+++ GeoLibNewSolution/GeoLib/GeoLib.ConsoleHost/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GeoLib.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,13 @@ namespace GeoLib.ConsoleHost
     {
         static void Main(string[] args)
         {
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+            hostGeoManager.Open();
+
+            Console.WriteLine("Service started. Press [Enter] to exit.");
+            Console.ReadLine();
+
+            hostGeoManager.Close();
         }
     }
 }
