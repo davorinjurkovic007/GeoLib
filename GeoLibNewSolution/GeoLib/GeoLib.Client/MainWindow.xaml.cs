bf76548e861fdcd5d93e7a865a6a316d65353568
Davorin <davorin.jurkovic@gmail.com>
2025-09-11 23:32:30 +0200

    GeoLib.Client

---
+++ GeoLibNewSolution/GeoLib/GeoLib.Client/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using GeoLib.Contracts;
+using GeoLib.Proxies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeoLib.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetInfo_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtZipCode.Text != "")
+            {
+               GeoClient proxy = new GeoClient();
+
+                try
+                {
+                    ZipCodeData data = proxy.GetZipInfo(txtZipCode.Text);
+                    if (data != null)
+                    {
+                        lblCity.Content = data.City;
+                        lblState.Content = data.State;
+                    }
+
+                    proxy.Close();
+                }
+                catch (FaultException ex)
+                {
+                    MessageBox.Show("Fault Excetpion: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnGetZipCodes_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtState.Text != null)
+            {
+                GeoClient proxy = new GeoClient();
+
+                IEnumerable<ZipCodeData> data = proxy.GetZips(txtState.Text);
+                if(data != null)
+                {
+                    lstZips.ItemsSource = data;
+                }
+
+                proxy.Close();
+            }
+
+        }
+
+        private void btnUpdateBatch_Click(object sender, RoutedEventArgs e)
+        {
+            List<ZipCityData> cityZipList = new List<ZipCityData>()
+            {
+                new ZipCityData() { ZipCode = "07035", City = "Bedrock" },
+                new ZipCityData() { ZipCode = "33033", City = "End of the World" }
+            };
+
+            try
+            {
+                GeoAdminClient proxy = new GeoAdminClient();
+
+                proxy.ClientCredentials.Windows.ClientCredential.UserName = "WcfUser";
+                proxy.ClientCredentials.Windows.ClientCredential.Password = "dotnet";
+
+                proxy.UpdateZipCity(cityZipList);
+
+                proxy.Close();
+
+                MessageBox.Show("Updated.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnPutBack_Click(object sender, RoutedEventArgs e)
+        {
+            List<ZipCityData> cityZipList = new List<ZipCityData>()
+            {
+                new ZipCityData() { ZipCode = "07035", City = "Lincoln Park" },
+                new ZipCityData() { ZipCode = "33033", City = "Homestead" }
+            };
+
+            try
+            {
+                GeoAdminClient proxy = new GeoAdminClient();
+
+                proxy.UpdateZipCity(cityZipList);
+
+                proxy.Close();
+
+                MessageBox.Show("Updated.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message );
+            }
+        }
+    }
+}
